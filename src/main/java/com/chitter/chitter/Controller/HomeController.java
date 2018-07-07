64947a87191da1f35ae7229d2d952f1ce1a337d3
Laura Petre <laura.d.petre@gmail.com>
2018-07-07 16:08:39 +0100

    Added basic controller to access index page

--- src/main/java/com/chitter/chitter/Controller/HomeController.java
+++ src/main/java/com/chitter/chitter/Controller/HomeController.java
@@ -1,4 +1,13 @@
 package com.chitter.chitter.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
 public class HomeController {
+
+    @RequestMapping(value="/")
+    public String index() {
+        return "index";
+    }
 }
